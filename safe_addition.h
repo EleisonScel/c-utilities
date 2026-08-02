a284d62deded0c3a8bd286f26ea0ca09ae121958
EleisonScel <EleisonNox@proton.me>
2026-08-02 19:13:03 +0500

    inclusion comment clarified

--- safe_addition.h
+++ safe_addition.h
@@ -36,7 +36,7 @@
 #ifndef SAFE_ADDITION_H
 #define SAFE_ADDITION_H
 
-#	include "assert_m.h"/* assert_m */
+#	include "assert_m.h"/* assert_check_m */
 
 #	include <stdint.h>	/* uint8_t	*/
 #	include <stddef.h>	/* size_t	*/
