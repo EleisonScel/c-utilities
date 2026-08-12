8621fcdfa620d41f99d4eb2ddfc83568fc291d2a
EleisonScel <EleisonNox@proton.me>
2026-08-12 19:58:40 +0500

    refactor: memory allocation and overflow errors are separated

--- safe_alloc.h
+++ safe_alloc.h
@@ -25,85 +25,127 @@
 
 #	include <stddef.h> /* size_t	*/
 #	include <stdlib.h> /* malloc	*/
+#	include <stdbool.h>/* bool		*/
 
 static inline size_t sa_check_array_bounds( size_t elements_amount, size_t element_size );
 
 /* Function:
  * safe version of malloc
  *
- * elements_amount	- desired amount of elements
- * element_size		- size of each element
+ * out_buffer_pointer	- holder to allocated memory
+ * elements_amount		- desired amount of elements
+ * element_size			- size of each element
  *
  * Returns:
- * pointer			- block was allocated
- * NULL				- invalid arguments, overflow, allocation failed
+ * true					- math is safe, memory allocation attempted
+ * false				- invalid arguments, overflow
  */
-static inline void * sa_malloc_array( size_t elements_amount, size_t element_size ) {
+static inline bool sa_malloc_array(
+		void * out_buffer_pointer, size_t elements_amount, size_t element_size
+	)
+{
 	size_t total_bytes = sa_check_array_bounds( elements_amount, element_size );
-	return (total_bytes == 0) ? NULL : malloc( total_bytes );
+
+	if (total_bytes == 0) {
+		void * null_pointer = NULL;
+		memcpy( out_buffer_pointer, &null_pointer, sizeof(void*) );
+		return false;
+	}
+
+	void * pointer_new = malloc(total_bytes);
+	memcpy( out_buffer_pointer, &pointer_new, sizeof(void*) );
+
+	return true;
 }
 
 /* Function:
  * safe version of calloc
  *
- * elements_amount	- desired amount of elements
- * element_size		- size of each element
+ * out_buffer_pointer	- holder to allocated memory
+ * elements_amount		- desired amount of elements
+ * element_size			- size of each element
  *
  * Returns:
- * pointer			- block was allocated
- * NULL				- invalid arguments, overflow, allocation failed
+ * true					- math is safe, memory allocation attempted
+ * false				- invalid arguments, overflow
  */
-static inline void * sa_calloc_array( size_t elements_amount, size_t element_size ) {
-	if ( sa_check_array_bounds( elements_amount, element_size ) == 0 )
-		return NULL;
-	return calloc( elements_amount, element_size );
+static inline bool sa_calloc_array(
+		void * out_buffer_pointer, size_t elements_amount, size_t element_size
+	)
+{
+	if ( sa_check_array_bounds( elements_amount, element_size ) == 0 ) {
+		void * null_pointer = NULL;
+		memcpy( out_buffer_pointer, &null_pointer, sizeof(void*) );
+		return false;
+	}
+
+	void * pointer_new = calloc( elements_amount, element_size );
+	memcpy( out_buffer_pointer, &pointer_new, sizeof(void*) );
+
+	return true;
 }
 
 /* Function:
  * safe version of realloc
  *
- * pointer_original	- original memory pointer
- * elements_amount	- desired amount of elements
- * element_size		- size of each element
+ * out_buffer_pointer	- holder to allocated memory
+ * pointer_original		- original memory pointer
+ * elements_amount		- desired amount of elements
+ * element_size			- size of each element
  *
  * Returns:
- * pointer			- block was reallocated
- * NULL				- invalid arguments, overflow, reallocation failed
+ * true					- math is safe, memory allocation attempted
+ * false				- invalid arguments, overflow
  */
-static inline void * sa_realloc_array(
-		void * pointer_original, size_t elements_amount, size_t element_size
+static inline bool sa_realloc_array(
+		void * out_buffer_pointer, void * pointer_original, size_t elements_amount,
+		size_t element_size
 	)
 {
 	size_t total_bytes = sa_check_array_bounds( elements_amount, element_size );
-	return (total_bytes == 0) ? NULL : realloc( pointer_original, total_bytes );
+
+	if (total_bytes == 0) {
+		void * null_pointer = NULL;
+		memcpy( out_buffer_pointer, &null_pointer, sizeof(void*) );
+		return false;
+	}
+
+	void * pointer_new = realloc( pointer_original, total_bytes );
+	memcpy( out_buffer_pointer, &pointer_new, sizeof(void*) );
+
+	return true;
 }
 
 /* Function:
  * safe version of realloc with nullification of a new data
  *
+ * out_buffer_pointer	- holder to allocated memory
  * pointer_original		- original memory pointer
  * elements_amount_old	- old amount of elements
  * elements_amount_new	- desired amount of elements
  * element_size			- size of each element
  *
  * Returns:
- * pointer				- block was reallocated, new space was nullifide
- * NULL					- invalid arguments, overflow, reallocation failed
+ * true					- math is safe, memory allocation attempted
+ * false				- invalid arguments, overflow
  */
-static inline void * sa_recalloc_array(
-		void * pointer_original, size_t elements_amount_old, size_t elements_amount_new,
-		size_t element_size
+static inline bool sa_recalloc_array(
+		void * out_buffer_pointer, void * pointer_original, size_t elements_amount_old,
+		size_t elements_amount_new, size_t element_size
 	)
 {
 	size_t total_bytes = sa_check_array_bounds( elements_amount_new, element_size );
-	if ( total_bytes == 0 )
-		return NULL;
+
+	if (total_bytes == 0) {
+		void * null_pointer = NULL;
+		memcpy( out_buffer_pointer, &null_pointer, sizeof(void*) );
+		return false;
+	}
 
 	void * pointer_new = realloc( pointer_original, total_bytes );
-	if ( pointer_new == NULL )
-		return NULL;
+	memcpy( out_buffer_pointer, &pointer_new, sizeof(void*) );
 
-	if ( elements_amount_new > elements_amount_old ) {
+	if ( pointer_new != NULL && elements_amount_new > elements_amount_old ) {
 		char * pointer_to_clean_from = (char *) pointer_new + (elements_amount_old * element_size);
 		memset(
 			pointer_to_clean_from,
@@ -112,7 +154,7 @@ static inline void * sa_recalloc_array(
 		);
 	}
 
-	return pointer_new;
+	return true;
 }
 
 /* Function:
